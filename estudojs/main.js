9cca9b64ae1e2b7a042aa59b393428ad5db93abb
Vando <vandodosreis2001@gmail.com>
2020-04-22 20:36:10 -0300

    Iniciando no js

---
+++ estudojs/main.js
@@ -0,0 +1,61 @@
+/*
+var count;
+var frase = "O marcos é burro";
+var frase2 = "Ele é pedofilo";
+var frase3 = "Ele inludi criaças"; 
+var d = new Date();
+for(count = 1; count <= 5; count++){
+    alert(count);
+    alert(d);
+    alert(frase);
+    alert(frase2);
+    alert(frase3);
+}
+*/
+
+/*
+var count = 0;
+while(count < 5){
+    console.log(count);
+    count = count + 1;
+}
+*/
+
+/*
+var idade = prompt("Qual a sua idade? ");
+if(idade >= 18){
+    alert("Você ja é de maior pode acessar esse conteudo");
+}else{
+    alert("Você não Tem idade para acessar esse conteudo");
+}
+*/
+
+/*
+var pessoas = [{nome:"vinicius", cor:"preto", idade:"26"},{nome:"viviane", cor:"preta", idade:"21"}]
+pessoas.push({nome:"vando", cor:"pardo", idade:"19"});
+console.log(pessoas);
+*/
+
+/*
+var pessoa = {nome:"vando", cor:"pardo", idade:"19"};
+console.log(pessoa);
+var lista = ["autor", "titulo", "laranja"];
+lista.pop();
+lista.push("uva");
+
+console.log(lista);
+*/
+
+/*
+var nome = "Vando dos reis";
+var n1 = 12;
+var n2 = 15;
+var frase = "O palmeiras não tem mundial";
+var frase2 = "são paulo não tem copa do brasil";
+console.log(nome);
+console.log(n1 + n2);
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+alert(frase.replace("palmeiras", "cruzeiro"));
+alert("minha primeira pagina");
+*/
